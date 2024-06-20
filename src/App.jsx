ab721499b85e2972ff0cda170a4304759ca76cb5
ovie012 <ovieemonefe012@gmail.com>
2024-06-20 09:21:02 +0100

    Header and Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header'
+import Footer from './Footer'
+import LandingPage from './LandingPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='text-7xl font-bold underline bg-cyan-500 text-purple-800 shadow-xl animate-bounce'>
-        Hello Swift Dispatch !!!
-      </div>
+      <Header />
+      <LandingPage />
+      <Footer />
     </>
   )
 }
