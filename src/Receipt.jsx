12f4d0bded214147ec10460946da2d3e7a9f978b
ovie012 <ovieemonefe012@gmail.com>
2024-07-13 11:00:18 +0100

    Receipt page started

---
+++ src/Receipt.jsx
@@ -0,0 +1,22 @@
+function Receipt () {
+    return (
+        <>
+            <main>
+                <div className="receipt-container">
+                    <section></section>
+                    <span></span>
+                    <section></section>
+                    <span></span>
+                    <section></section>
+                    <span></span>
+                    <section></section>
+                    <span></span>
+                    <section></section>
+                    <button></button>
+                </div>
+            </main>
+        </>
+    )
+};
+
+export default Receipt;
