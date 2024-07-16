84ebcebf296b046734629b32bec421568bb76a1b
Aisha-zen <bjaisha9@gmail.com>
2024-07-16 12:36:41 +0100

    about page

--- src/About.jsx
+++ src/About.jsx
@@ -1,15 +1,11 @@
 import "./App.css";
 import Header from "./Header";
 import Footer from "./Footer";
+
 function About() {
   return (
     <>
       <Header />
-      <div className="min-h-screen">
-        <div>
-          <h1>About</h1>
-        </div>
-      </div>
       <Footer />
     </>
   );
